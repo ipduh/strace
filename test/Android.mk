858a5c77bfa0aef527442d66432ab217b2e9beb8
Elliott Hughes <enh@google.com>
2014-02-28 15:58:12 -0800

    Build the strace tests.
    (Optionally, of course.)
    
    Change-Id: I8f4a98f38aeb1899290a72b9b4fdbd86c65cf44a

---
+++ test/Android.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := $(call my-dir)
+
+define declare-strace-test-target
+  include $(CLEAR_VARS)
+  LOCAL_SRC_FILES := $(1)
+  LOCAL_MODULE := strace-$(basename $(1))-test
+  LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+  LOCAL_MODULE_TAGS := tests
+  LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
+  include $(BUILD_EXECUTABLE)
+endef
+
+$(foreach file, $(wildcard $(LOCAL_PATH)/*.c), \
+    $(eval $(call declare-strace-test-target,$(notdir $(file)))))
